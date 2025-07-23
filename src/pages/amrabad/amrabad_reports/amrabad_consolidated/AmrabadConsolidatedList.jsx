447820357f4d97669cdd34c581810618206900d6
swetha <swetha.putta@vmaxindia.com>
2025-07-23 18:26:52 +0530

    report action columns

--- src/pages/amrabad/amrabad_reports/amrabad_consolidated/AmrabadConsolidatedList.jsx
+++ src/pages/amrabad/amrabad_reports/amrabad_consolidated/AmrabadConsolidatedList.jsx
@@ -183,26 +183,26 @@ function AmrabadConsolidatedList() {
       headerClass: "text-blue-v2",
       valueFormatter: (params) => (params.value ? "Success" : "Failed"),
     },
-    {
-      headerName: "Actions",
-      field: "actions",
-      cellRenderer: (params) => (
-        <div style={{ display: "flex align-center", gap: "0.5rem" }}>
-          <NavLink
-            end
-            to={`/entity-bookings/view-details/${params.data.bookingID}`}
-            onClick={() => {
-              setisAmrabadCompleteBookings(true);
-            }}
-            className="bg-gray-100 text-white px-4 py-2 rounded-md hover:bg-gray-200 hover:text-gray-100 transition"
-          >
-            <span className="text-blue-v2"> Booking Details</span>
-          </NavLink>
-        </div>
-      ),
-      flex: 1,
-      headerClass: "text-blue-v2",
-    },
+    // {
+    //   headerName: "Actions",
+    //   field: "actions",
+    //   cellRenderer: (params) => (
+    //     <div style={{ display: "flex align-center", gap: "0.5rem" }}>
+    //       <NavLink
+    //         end
+    //         to={`/entity-bookings/view-details/${params.data.bookingID}`}
+    //         onClick={() => {
+    //           setisAmrabadCompleteBookings(true);
+    //         }}
+    //         className="bg-gray-100 text-white px-4 py-2 rounded-md hover:bg-gray-200 hover:text-gray-100 transition"
+    //       >
+    //         <span className="text-blue-v2"> Booking Details</span>
+    //       </NavLink>
+    //     </div>
+    //   ),
+    //   flex: 1,
+    //   headerClass: "text-blue-v2",
+    // },
   ]);
   return (
     <>
