f265418e03f554b80e06f0315131641f4ff0dc1d
Venkat Tiruvaipati <venkat.t@vmaxindia.com>
2025-08-11 10:41:58 +0530

    changes

--- src/pages/amrabad/user/confirmedDetails/components/BookingDetails.jsx
+++ src/pages/amrabad/user/confirmedDetails/components/BookingDetails.jsx
@@ -1,10 +1,11 @@
 const BookingDetails = ({ booking }) => {
+  
   return (
     <div className="">
       <h2 className="text-base sm:text-lg font-semibold mb-3 sm:mb-4 text-black">Booking details:</h2>
       <div className="text-xs sm:text-sm space-y-2">
         <div>
-          <span className="font-bold text-black">Package:</span> {booking.package}
+          <span className="font-bold text-black">Package:</span> {booking.packageName}
         </div>
         <div>
           <span className="font-bold text-black">Check-in:</span> {booking.checkIn}
