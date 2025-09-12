2cb0d61e2832f697eb7633c8c15107588055ff63
Swetha Putta <swetha.putta@vmaxindia.com>
2025-09-12 10:02:54 +0530

    consolidated report

--- src/constants/permissions.js
+++ src/constants/permissions.js
@@ -133,6 +133,7 @@ export const RtcAdmin = [
   "bus-pass-total-transaction",
   "bus-pass-booking-report",
   "bus-pass-refund-report",
+  "intercity-consolidated-report",
   "intercity-total-transaction",
   "intercity-user-report",
   "intercity-refund-report",
