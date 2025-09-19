7eea518ea4c083d8c8c4a03d12c6e11eebdcdaea
Ramana <ramana.m@vmaxindia.com>
2025-09-19 11:09:37 +0530

    amrabad and rtc code merged

--- src/routes/routesConfig.jsx
+++ src/routes/routesConfig.jsx
@@ -191,7 +191,6 @@ import IntercityConsolidatedReport from "../components/rtc/rtc_reports/intercity
 import IntercityIndividualReport from "../components/rtc/rtc_reports/intercity_reports/IntercityIndividual/IntercityIndividualReport";
 import IntercityPaymentTransactionsReport from "../components/rtc/rtc_reports/intercity_reports/intercity_payment_transactions/IntercityPaymentTransactionsReport";
 import IntercityTotalTransactionTrackOrder from "../components/rtc/rtc_reports/intercity_reports/intercityTotalTransactionTrackOrder";
-import IntercityTicketView from "../components/rtc/rtc_reports/intercity_reports/intercity_ticket_view/IntercityTicketView";
 
 export const routes = [
   {
