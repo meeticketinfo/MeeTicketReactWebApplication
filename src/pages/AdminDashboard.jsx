0a798242b38b358fd8610860bdd6fcda3d925cb4
Venkat Tiruvaipati <venkat.t@vmaxindia.com>
2025-10-28 17:13:14 +0530

    changes

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -244,7 +244,7 @@ function AdminDashboard() {
 
   const cardsToDisplay =
     roleDetails?.name === "ROLE_ADMIN" ||
-    roleDetails?.name === "ROLE_ZOOPARKADMIN"
+    roleDetails?.name === "ROLE_ZOOPARKADMIN"|roleDetails?.name === "ROLE_COUNTERLOGIN"
       ? dashboardCardsCountByRole
       : dashboardCards;
 
