a8788829541224592a501b77aa8d9386ef6c0b20
Ramana <ramana.m@vmaxindia.com>
2025-11-04 15:41:17 +0530

    baseurl

--- src/constants/apiEndpoints.js
+++ src/constants/apiEndpoints.js
@@ -1,12 +1,12 @@
 import useAuthStore from "../store/authStore";
 
 /// dev park
-// export const API_BASE_URL =
-//   "https://meeticketdevui.vmaxtechservices.help/parkapi/api/";
+export const API_BASE_URL =
+  "https://meeticketdevui.vmaxtechservices.help/parkapi/api/";
 
 //  PARK UAT
-export const API_BASE_URL =
-  "https://meeticket.telangana.gov.in/parkuatapi/api/";
+// export const API_BASE_URL =
+//   "https://meeticket.telangana.gov.in/parkuatapi/api/";
 
 //  paynow UAT
 export const PAYNOW_API_BASE_URL =
@@ -40,12 +40,12 @@ export const TOURISM_API_BASE_URL =
 //   "https://meeticketdevui.vmaxtechservices.help/webservices/v1/";
 
 // RTC dev
-// export const RTC_API_BASE_URL =
-//   "https://meeticketdevui.vmaxtechservices.help/rtcbuspassapi/v1/";
+export const RTC_API_BASE_URL =
+  "https://meeticketdevui.vmaxtechservices.help/rtcbuspassapi/v1/";
 
 // RTC UAT
-export const RTC_API_BASE_URL =
-  "https://meeticket.telangana.gov.in/rtcbuspassapi/v1/";
+// export const RTC_API_BASE_URL =
+//   "https://meeticket.telangana.gov.in/rtcbuspassapi/v1/";
 
 
 //RTC Intercity
