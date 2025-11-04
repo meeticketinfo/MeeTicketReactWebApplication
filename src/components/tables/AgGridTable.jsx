0653ae8b42e4ecaf6fa1719f42e91635a1213404
swetha <putta.swetha@vmaxindia.com>
2025-11-04 15:55:27 +0530

    pos masters bug

--- src/components/tables/AgGridTable.jsx
+++ src/components/tables/AgGridTable.jsx
@@ -281,6 +281,8 @@ console.log(params);
               <option value={100}>100</option>
               <option value={500}>500</option>
               <option value={1000}>1000</option>
+              <option value={20000}>20000</option>
+              <option value={20000}>50000</option>
             </select>
           </div>
           <ReactPaginate
